090482dff50b3b7c6c2aaadfad9632a0518923e2
ronakkany <ronny.wels@gmx.de>
2021-10-26 16:59:32 +0200

    refactoring tests

--- src/test/scala/de/commons/lib/spark/environments/SparkEnvironmentSpec.scala
+++ src/test/scala/de/commons/lib/spark/environments/SparkEnvironmentSpec.scala
@@ -29,33 +29,35 @@ class SparkEnvironmentSpec extends TestSpec with SparkTestSupport {
     "applyR" in {
       val env = new SparkEnvironment(configuration, logger)
       def f(spark: SparkSession, logger: Logger): Int = 1
-
       val program = env
         .applyR(ZIO.environment[ZEnv] >>> Task[(SparkSession, Logger) => Int](f))
         .provideCustomLayer(ZEnv.live)
+
       runtime.unsafeRun(program) mustBe 1
     }
     "liftF" in {
       def f(a: String)(spark: SparkSession, logger: Logger): Int = 1
-
       val env = new SparkEnvironment(configuration, logger)
       val program: Task[Int] = env.liftF(f)(Task("foo")).flatten
+
       whenReady(program)(_ mustBe Right(1))
     }
     "liftR" in {
       def f(a: Int)(spark: SparkSession, logger: Logger): Int = 1
-
       val env = new SparkEnvironment(configuration, logger)
       val program = env.liftR(f)(Task(1)).flatten
+
       whenReady(program)(_ mustBe Right(1))
     }
     "raiseError" in {
       val env = new SparkEnvironment(configuration, logger)
+
       whenReady(env.raiseError[String](new Throwable))(_.isLeft mustBe true)
     }
     "raiseErrorR" in {
       val env = new SparkEnvironment(configuration, logger)
       val runtime = zio.Runtime(env, Platform.default)
+
       Try(runtime.unsafeRun(env.raiseErrorR(new Throwable))).toOption mustBe None
     }
   }
