eb053b7f501f96b29926959ab979f1e80be44e35
ronakkany <ronny.wels@gmx.de>
2021-11-03 11:58:06 +0100

    add contravariance test, add contravariant operator

--- src/test/scala/de/commons/lib/spark/SparkRunnableSpec.scala
+++ src/test/scala/de/commons/lib/spark/SparkRunnableSpec.scala
@@ -5,6 +5,8 @@ import de.commons.lib.spark.environments.SparkR.SparkEnvironment
 import de.commons.lib.spark.errors.SparkRunnableThrowable
 import zio.{Task, ZIO}
 
+import scala.util.Random
+
 class SparkRunnableSpec extends TestSpec with SparkTestSupport {
 
   "SparkRZIO#run" must {
@@ -31,6 +33,17 @@ class SparkRunnableSpec extends TestSpec with SparkTestSupport {
 
       val runnable = SparkRZIO[SparkEnvironment, Any, String](io).run
       whenReady(runnable.provide(new SparkEnvironment(configuration, logger)))(_ mustBe Right("foo"))
+    }
+    "return a string -- contravariant case" in withSparkSession { implicit spark => implicit logger =>
+      trait RandomTrait[T] {
+        def random: Task[T]
+      }
+      class RandomInt extends RandomTrait[Int] {
+        override def random: Task[Int] = Task(Random.nextInt())
+      }
+
+      val runnable: SparkZIO[RandomInt, String] = SparkZIO[RandomTrait[Int], String](Task.succeed("foo"))
+      whenReady(runnable.run.provide(new RandomInt))(_ mustBe Right("foo"))
     }
   }
 
