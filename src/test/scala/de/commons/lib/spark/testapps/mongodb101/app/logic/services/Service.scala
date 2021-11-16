196dde8c8223dc56143b6a63834de46bb21a48e2
ronakkany <ronny.wels@gmx.de>
2021-11-16 13:34:25 +0100

    remove inject annotation

--- src/test/scala/de/commons/lib/spark/testapps/mongodb101/app/logic/services/Service.scala
+++ src/test/scala/de/commons/lib/spark/testapps/mongodb101/app/logic/services/Service.scala
@@ -5,9 +5,7 @@ import de.commons.lib.spark.environments.io.SparkDataFrameReader.DataFrameMongoD
 import de.commons.lib.spark.testapps.mongodb101.app.logic.collections.Agent
 import org.apache.spark.sql.{Dataset, SparkSession}
 
-import javax.inject.Inject
-
-final class Service @Inject()(reader: DataFrameMongoDbReader) {
+final class Service(reader: DataFrameMongoDbReader) {
 
   def select(agentCodes: NonEmptyList[String])(implicit sparkSession: SparkSession): Dataset[Agent] =
     Agent.select(reader)(agentCodes)
