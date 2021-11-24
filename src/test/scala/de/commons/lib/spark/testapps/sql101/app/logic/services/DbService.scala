605ab04de0108f48a19547d4077c967cf60ea4b8
ronnywels <info@lightning-payments.de>
2021-11-24 19:07:34 +0100

    refactoring

--- src/test/scala/de/commons/lib/spark/testapps/sql101/app/logic/services/DbService.scala
+++ src/test/scala/de/commons/lib/spark/testapps/sql101/app/logic/services/DbService.scala
@@ -1,9 +1,9 @@
 package de.commons.lib.spark.testapps.sql101.app.logic.services
 
-import de.commons.lib.spark.services.Spark.HasSpark
-import de.commons.lib.spark.services.SparkT
 import de.commons.lib.spark.io.SparkDataFrameReader.{DataFrameQueryReader, DatabaseReader}
 import de.commons.lib.spark.io.SparkDataFrameWriter.{DataFrameDatabaseWriter, DatabaseInsert}
+import de.commons.lib.spark.services.Spark.HasSpark
+import de.commons.lib.spark.services.SparkT
 import de.commons.lib.spark.testapps.sql101.app.logic.joins.JoinedAgentsOrdersCustomers
 import de.commons.lib.spark.testapps.sql101.app.logic.tables.{Agent, Customer, Order}
 import org.apache.spark.sql.Dataset
@@ -19,11 +19,11 @@ private[sql101] final class DbService(
   private val databaseInsert: DataFrameDatabaseWriter = DatabaseInsert(url, properties)
 
   def getAgents: ZIO[HasSpark, Throwable, Dataset[Agent]] =
-    for {
-      env <- sparkIO
-      _   <- env.loggerM.map(_.debug("select all agents"))
-      ds  <- env.sparkM.map(implicit spark => Agent.select(databaseReader))
-    } yield ds
+    sparkIO.flatMap(_.sparkWithLogger).map {
+      case (spark, logger) =>
+        logger.debug("select all agents")
+        Agent.select(databaseReader)(spark)
+    }
 
   def insertAgents(ds: Dataset[Agent]): ZIO[HasSpark, Throwable, Unit] =
     sparkIO
@@ -31,24 +31,24 @@ private[sql101] final class DbService(
       .map(_ => Agent.insert(ds)(databaseInsert))
 
   def getCustomers: ZIO[HasSpark, Throwable, Dataset[Customer]] =
-    for {
-      env <- sparkIO
-      _   <- env.loggerM.map(_.debug("select all customers"))
-      ds  <- env.sparkM.map(implicit spark => Customer.select(databaseReader))
-    } yield ds
+    sparkIO.flatMap(_.sparkWithLogger).map {
+      case (spark, logger) =>
+        logger.debug("select all customers")
+        Customer.select(databaseReader)(spark)
+    }
 
   def getOrders: ZIO[HasSpark, Throwable, Dataset[Order]] =
-    for {
-      env <- sparkIO
-      _   <- env.loggerM.map(_.debug("select all orders"))
-      ds  <- env.sparkM.map(implicit spark => Order.select(databaseReader))
-    } yield ds
+    sparkIO.flatMap(_.sparkWithLogger).map {
+      case (spark, logger) =>
+        logger.debug("select all orders")
+        Order.select(databaseReader)(spark)
+    }
 
   def getAgentsStatistics: ZIO[HasSpark, Throwable, Dataset[JoinedAgentsOrdersCustomers]] =
-    for {
-      env <- sparkIO
-      _   <- env.loggerM.map(_.debug("join agents via order via customers"))
-      ds  <- env.sparkM.map(implicit spark => JoinedAgentsOrdersCustomers.select(databaseReader))
-    } yield ds
+    sparkIO.flatMap(_.sparkWithLogger).map {
+      case (spark, logger) =>
+        logger.debug("join agents via order via customers")
+        JoinedAgentsOrdersCustomers.select(databaseReader)(spark)
+    }
 
 }
