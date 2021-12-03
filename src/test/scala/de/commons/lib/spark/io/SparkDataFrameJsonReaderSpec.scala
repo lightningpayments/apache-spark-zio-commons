5c116d918ca6b3ba7b77ff383c3f6464c85d8f0b
ronnywels <info@lightning-payments.de>
2021-12-03 17:12:05 +0100

    test JsonReaderDataset

--- src/test/scala/de/commons/lib/spark/io/SparkDataFrameJsonReaderSpec.scala
+++ src/test/scala/de/commons/lib/spark/io/SparkDataFrameJsonReaderSpec.scala
@@ -11,20 +11,24 @@ class SparkDataFrameJsonReaderSpec extends TestSpec with SparkTestSupport { self
   private case class Dummy(value: Long)
   private implicit val encoders: Encoder[Dummy] = Encoders.product[Dummy]
 
-  "SparkDataFrameSqlReader#DatabaseReader" must {
+  "SparkDataFrameSqlReader#JsonReaderPath" must {
     "return a json from filet" in withSparkSession { implicit spark => _ =>
       val path = Paths.get(self.getClass.getResource("/json/dummy.json").getPath)
-
       val reader = SparkDataFrameReader.JsonReaderPath(path.normalize().toString)
-      val program = ZIO.tupled(
-        ZIO(reader.run.as[Dummy].collect().toList),
-        ZIO(spark.createDataset(Dummy(1) :: Nil).collect().toList)
-      )
 
-      whenReady(program) {
-        case Left(ex) => fail(ex)
-        case Right(tupled) => tupled._1 mustBe tupled._2
-      }
+      whenReady(ZIO(reader.run.as[Dummy].collect().toList))(_ mustBe Right(Dummy(1) :: Nil))
+    }
+  }
+
+  "SparkDataFrameSqlReader#JsonReaderDataset" must {
+    "return a json from filet" in withSparkSession { implicit spark => _ =>
+      import spark.implicits._
+
+      val json = """[{"value": 1}]"""
+      val ds = spark.createDataset(json :: Nil)
+      val reader = SparkDataFrameReader.JsonReaderDataset(ds)
+
+      whenReady(ZIO(reader.run.as[Dummy].collect().toList))(_ mustBe Right(Dummy(1) :: Nil))
     }
   }
 
