701b69844492c138d59d4132c2d20fe256bc7c4d
ronnywels <info@lightning-payments.de>
2021-12-07 20:27:10 +0100

    version

--- src/main/scala/de/commons/lib/spark/RTFuture.scala
+++ src/main/scala/de/commons/lib/spark/RTFuture.scala
@@ -2,6 +2,7 @@ package de.commons.lib.spark
 
 import cats.effect.{ExitCode, IO, IOApp}
 
+import java.time.LocalDateTime
 import scala.concurrent.ExecutionContext.Implicits._
 import scala.concurrent.duration.DurationInt
 import scala.concurrent.{Await, Future}
@@ -22,44 +23,44 @@ object RTFuture extends IOApp {
 
   override def run(args: List[String]): IO[ExitCode] = {
     val a0 =
-      Future(Thread.sleep(Random.nextInt(100)))
-        .flatMap(_ => Future(println("a0")))
+      Future(println("a0" + LocalDateTime.now()))
+        .flatMap(_ => Future(Thread.sleep(Random.nextInt(100))))
         .flatMap(_ => Future(subtractSalesTax(100)))
     val a1 =
-      Future(Thread.sleep(Random.nextInt(100)))
-        .flatMap(_ => Future(println("a1")))
+      Future(println("a1" + LocalDateTime.now()))
+        .flatMap(_ => Future(Thread.sleep(Random.nextInt(100))))
         .flatMap(_ => Future(subtractSalesTax(100) + subtractSalesTax(100)))
     val a2 = for {
+      _ <- Future(println("a2" + LocalDateTime.now()))
       a <- a0
       b <- a0
-      _ <- Future(println("a2"))
     } yield a + b
     println(s"***$noReferentialTransparency***")
-    println("a", Await.result(a1, 5.seconds))
-    println("b", Await.result(a2, 5.seconds))
+    println("a1", Await.result(a1, 5.seconds))
+    println("a2", Await.result(a2, 5.seconds))
     println()
 
     val io0 =
       IO.fromFuture(
+        IO(println("io0" + LocalDateTime.now())) *>
         IO(Thread.sleep(Random.nextInt(100))) *>
-        IO(println("io0")) *>
         IO(Future(subtractSalesTax(100)))
       )
     val io1 =
       IO.fromFuture(
+        IO(println("io1" + LocalDateTime.now())) *>
         IO(Thread.sleep(Random.nextInt(100))) *>
-        IO(println("io1")) *>
         IO(Future(subtractSalesTax(100) + subtractSalesTax(100)))
       )
-    val io2 = io0.flatMap(x => io0.map(x + _))
+    val io2 = IO(println("io2" + LocalDateTime.now())) *> io0.flatMap(x => io0.map(x + _))
     (for {
-      _  <- IO(println("io last"))
-      n1 <- io1
-      n2 <- io2
+      _  <- IO(println("io2" + LocalDateTime.now()))
+      o0 <- io1
+      o1 <- io2
       _  <- IO {
         println(s"***$withReferentialTransparency***")
-        println(n1)
-        println(n2)
+        println("io1", o0)
+        println("io2", o1)
         println()
       }
     } yield ()).as(ExitCode.Success)
